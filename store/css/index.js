ec8f8b80a07df04ea8e0c33e501adfb605b1fea4
develophiro <abc.umd.first1@gmail.com>
2023-12-01 03:39:02 +0900

    kaizenn3

---
+++ store/css/index.js
@@ -0,0 +1,31 @@
+const on =()=> {
+    //今のカウントを調べる
+    var count = document.getElementById("kazu1").innerText;
+    //カウントを1増やす
+    document.getElementById("kazu1").innerText = parseInt(count) + 1;
+
+}
+
+const on2 =()=> {
+    //今のカウントを調べる
+    var count = document.getElementById("kazu2").innerText;
+    //カウントを1増やす
+    document.getElementById("kazu2").innerText = parseInt(count) + 1;
+
+}
+
+const on3 =()=> {
+    //今のカウントを調べる
+    var count = document.getElementById("kazu3").innerText;
+    //カウントを1増やす
+    document.getElementById("kazu3").innerText = parseInt(count) + 1;
+
+}
+
+const on4 =()=> {
+    //今のカウントを調べる
+    var count = document.getElementById("kazu4").innerText;
+    //カウントを1増やす
+    document.getElementById("kazu4").innerText = parseInt(count) + 1;
+
+}
