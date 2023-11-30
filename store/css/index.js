b01a46589ef10015ddaf1a54fe03704f612333e4
develophiro <abc.umd.first1@gmail.com>
2023-12-01 06:01:37 +0900

    kaizenn4

--- store/css/index.js
+++ store/css/index.js
@@ -45,7 +45,7 @@ const on4 =()=> {
     document.getElementById("image").src = img_src[i];
   }
      
-  var img_src = new Array("img/bad.jpg","img/baki.jpg");
+  var img_src2 = new Array("img/baki.jpg","img/syousai2.jpg");
   var i = 0;
 
   function openImg2() {
@@ -55,5 +55,31 @@ const on4 =()=> {
     } else {
       i ++;
     }
-    document.getElementById("image2").src = img_src[i];
+    document.getElementById("image2").src = img_src2[i];
+  }
+
+
+  var img_src3 = new Array("img/esta-.jpg","img/syousai3.jpg");
+  var i = 0;
+
+  function openImg3() {
+
+    if (i == 1) {
+      i = 0;
+    } else {
+      i ++;
+    }
+    document.getElementById("image3").src = img_src3[i];
+  }
+
+  var img_src4 = new Array("img/sennki.jpg","img/syousai4.jpg");
+
+  function openImg4() {
+
+    if (i == 1) {
+      i = 0;
+    } else {
+      i ++;
+    }
+    document.getElementById("image4").src = img_src4[i];
   }
