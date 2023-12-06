673b3652701b252232f2334e7debe89dfd87819a
develophiro <151043622+develophiro@users.noreply.github.com>
2023-12-07 04:58:07 +0900

    Delete store/js

--- store/js/index.js
+++
@@ -1,7 +0,0 @@
-const buy =()=> {
-    //今のカウントを調べる
-    var count = document.getElementById("cnt").innerText;
-    //カウントを1増やす
-    document.getElementById("cnt").innerText = parseInt(count) + 1;
-
-}
