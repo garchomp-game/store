4c7b5cad47b448c9bd4768e7f0b1caa878008205
develophiro <abc.umd.first1@gmail.com>
2023-12-07 05:30:50 +0900

    commit1

--- store/js/index.js
+++ store/js/index.js
@@ -1,7 +1,38 @@
-const buy =()=> {
-    //今のカウントを調べる
-    var count = document.getElementById("cnt").innerText;
-    //カウントを1増やす
-    document.getElementById("cnt").innerText = parseInt(count) + 1;
 
+
+
+const on=(num)=> {
+    //今のカウントを調べる
+   
+    var count = document.getElementById("kazu"+num).innerText;
+    //カウントを1増やす
+    document.getElementById("kazu"+num).innerText = parseInt(count) + 1;
+    
 }
+    
+  
+      
+  
+  
+  
+  
+  var i = 0; 
+
+  var img_src = new Array("img/bad.jpg","img/baki.jpg","img/esta-.jpg","img/sennki.jpg");
+  function openImg(num) {
+  
+    if (i == 1) {
+     // サムネ表示
+     document.getElementById("image"+num).src=(img_src[num-1]);
+      i = 0;
+
+    } else  {
+      // 詳細表示
+      document.getElementById("image"+num).src =`img/syousai${num}.jpg`;
+      i++;
+     
+    }
+  }
+    
+        
+  
