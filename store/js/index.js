cb73343b9998ca139a8b882d6256e210e17be2f9
develophiro <abc.umd.first1@gmail.com>
2023-12-09 01:49:27 +0900

    commit6

--- store/js/index.js
+++ store/js/index.js
@@ -1,32 +1,32 @@
 
- const onClickNice=(num)=> {
+  const onClickNice=(num)=> {
 //今のカウントを調べる
 
-   var count = document.getElementById("kazu"+num).innerText;
+    var count = document.getElementById("kazu"+num).innerText;
     //カウントを1増やす
-   document.getElementById("kazu"+num).innerText = parseInt(count) + 1;
+    document.getElementById("kazu"+num).innerText = parseInt(count) + 1;
 
 }
     
- var isShow = false; 
+  var isShow = false; 
 
- var img_src = new Array("img/bad.jpg","img/baki.jpg","img/esta-.jpg","img/sennki.jpg");
- function openImg(num) {
+  var img_src = new Array(
+    "img/bad.jpg",
+    "img/baki.jpg",
+    "img/esta-.jpg",
+    "img/sennki.jpg"
+  );
+  function openImg(num) {
   
-   if (isShow) {
+    if (isShow) {
      // サムネ表示
-   document.getElementById("image"+num).src=(img_src[num-1]);
-   
- 
-   } else  {
+    document.getElementById("image"+num).src=(img_src[num-1]);
+    } else  {
    // 詳細表示
-   document.getElementById("image"+num).src =`img/syousai${num}.jpg`;
-   
-   
-
-  }
+      document.getElementById("image"+num).src =`img/syousai${num}.jpg`;
+    }
     isShow= !isShow;
- }
+}
     
         
   
